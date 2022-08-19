89eb31950fd9d82e28bd2a1fd79ff689cad13b3d
Leonard Imbert <imbert@subatech.in2p3.fr>
2022-08-19 11:50:44 +0200

    [Release] Update version number 0.3.1 -> 0.3.2

--- python/__init__.py
+++ python/__init__.py
@@ -1,6 +1,6 @@
 """Collection of tools to manipulate DataMo files in python
 """
-__version__ = '0.3.1'
+__version__ = '0.3.2'
 __author__ = 'Leonard Imbert <imbert@subatech.in2p3.fr>'
 
 from .DataMoReader import DataMoReader, Entry, ReadableType, Error, MalformedFile
